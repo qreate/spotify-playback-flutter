d021f46a5b3e5c47522d90cac44de957852dc9db
Joran Dob <joran998@gmail.com>
2019-04-10 17:34:12 +0200

    Added some base implementations for AUTH on iOS

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -2,7 +2,15 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, SPTSessionManagerDelegate {
+    let SpotifyClientID = ""
+    let SpotifyRedirectURL = URL(string: "")!
+    
+    lazy var configuration = SPTConfiguration(
+        clientID: SpotifyClientID,
+        redirectURL: SpotifyRedirectURL
+    )
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
@@ -10,4 +18,13 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("success", session)
+    }
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("fail", error)
+    }
+    func sessionManager(manager: SPTSessionManager, didRenew session: SPTSession) {
+        print("renewed", session)
+    }
 }
--- example/ios/Runner/Runner-Bridging-Header.h
+++ example/ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,2 @@
 #import "GeneratedPluginRegistrant.h"
+#import <SpotifyiOS/SpotifyiOS.h>
