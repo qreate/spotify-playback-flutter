326db0397275ca97b27f0b1444c91713effc1919
Joran Dob <joran998@gmail.com>
2019-04-22 10:51:35 +0200

    Changed description for getAuthToken
    Feel free to change this description to something more specific.

--- lib/spotify_playback.dart
+++ lib/spotify_playback.dart
@@ -130,7 +130,7 @@ class SpotifyPlayback {
     return "spotify:image:" + imageLink.replaceRange(0, imageLink.lastIndexOf("/") + 1, "");
   }
 
-  /// This method is used to get an image by the provided imageURI and returns a Uint8List(MemoryImage)
+  /// This method is used to get the authToken
   static Future<String> getAuthToken(
       {@required String clientId, @required String redirectUrl}) async {
     final String token = await _channel.invokeMethod(
